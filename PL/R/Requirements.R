2357cd508ee0ae14f12af7109fcfb2eca665b1d5
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2018-10-18 18:13:05 +0100

    added a req for R

--- PL/R/Requirements.R
+++ PL/R/Requirements.R
@@ -1,13 +1,15 @@
 packages = c(
+    "data.table",
+    "dplyr",
+    "filehash",             # key-value database
     "ggplot2",
     "plyr",                 # tools for splitting, applying, and combining data
-    "dplyr",
-    "ProjectTemplate",      # automate creation of new projects
-    "filehash",             # key-value database
-    "tikzDevice",           # graphics output in LaTeX format
-    "varhandle",
-    "psych"                 # tools for psychological, psychometric, and personality research,
+    "psych",                # tools for psychological, psychometric, and personality research,
                             #   including read.clipboard.csv()
+    "ProjectTemplate",      # automate creation of new projects
+    "tidyverse",
+    "tikzDevice",           # graphics output in LaTeX format
+    "varhandle"
 )
 
 install.packages(packages, repos="http://cran.rstudio.com/")
