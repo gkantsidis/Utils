5c044f65336afcc40e63b831b9dbb032bf69c523
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2019-03-15 19:28:14 +0000

    starting to work on GeoSpatial structures

---
+++ PL/DotNet/FSHelper/Generic/Spatial/GeoLocation.fs
@@ -0,0 +1,13 @@
+﻿namespace CGFSHelper.Units
+
+module GeoLocation =
+    open Microsoft.FSharp.Core
+
+    module FloatImplemenation =
+        let private error : float<m> = LanguagePrimitives.FloatWithMeasure 0.001
+        let private earth_radius : float<m> = LanguagePrimitives.FloatWithMeasure 6371000.0
+
+        [<Measure>] type radians
+        [<Measure>] type degrees
+
+        type Radians = float<radians>
