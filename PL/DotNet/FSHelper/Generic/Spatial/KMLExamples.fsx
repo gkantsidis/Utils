8117bc0d351d6ac0d11a317ace9c2b8f4bd75fd2
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2019-03-29 22:18:00 +0000

    added sample for KML and script

---
+++ PL/DotNet/FSHelper/Generic/Spatial/KMLExamples.fsx
@@ -0,0 +1,13 @@
+﻿#load @"../Collections.fs"
+#load "GeoLocation.fs"
+#load "KeyholeMarkupLanguage.fs"
+
+open System.IO
+
+let samples =
+    let path = Path.Combine(__SOURCE_DIRECTORY__, "..", "..", "..", "..", "..", "Data", "KMLExamples")
+    let info = DirectoryInfo(path)
+    info.FullName
+// assert(Directory.Exists(samples))
+
+let content = File.ReadAllText(Path.Combine(samples, "KML_Samples.kml"))
