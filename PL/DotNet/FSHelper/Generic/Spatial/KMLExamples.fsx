1d2859fecd5d399302d5bd674d988a9f6067e71d
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2019-04-02 18:30:36 +0100

    attempts to generate types from xsd automatically; goal is to use the xsd definition of KML

--- PL/DotNet/FSHelper/Generic/Spatial/KMLExamples.fsx
+++ PL/DotNet/FSHelper/Generic/Spatial/KMLExamples.fsx
@@ -1,8 +1,12 @@
-﻿#load @"../Collections.fs"
+﻿#I @"../../../.paket/load/net47/"
+#load @"FSharp.Data.fsx"
+
+#load @"../Collections.fs"
 #load "GeoLocation.fs"
 #load "KeyholeMarkupLanguage.fs"
 
 open System.IO
+open FSharp.Data
 
 let samples =
     let path = Path.Combine(__SOURCE_DIRECTORY__, "..", "..", "..", "..", "..", "Data", "KMLExamples")
@@ -11,3 +15,10 @@ let samples =
 // assert(Directory.Exists(samples))
 
 let content = File.ReadAllText(Path.Combine(samples, "KML_Samples.kml"))
+
+type XHtml = XmlProvider<Schema="../../../../../Data/Schemas/xhtml.xsd">
+//type Namespace = XmlProvider<Schema="../../../../../Data/Schemas/namespace.xsd">
+//type Atom = XmlProvider<Schema="../../../../../Data/Schemas/atom3.xsd">
+//type Schema = XmlProvider<Schema="../../../../../Data/Schemas/ogckml22.xsd">
+// type Schema = XmlProvider<Schema="http://schemas.opengis.net/kml/2.2.0/ogckml22.xsd">
+
