9dce8df91a9bb6c8730f5f01349e3e98c561deba
Christos Gkantsidis <gkantsidis@gmail.com>
2019-04-02 10:10:45 +0100

    basically functionality seems to work for converting Coordinates to GeoHash

--- PL/DotNet/FSHelper/Generic/Spatial/GeoHash.fs
+++ PL/DotNet/FSHelper/Generic/Spatial/GeoHash.fs
@@ -3,7 +3,15 @@
 module GeoHash =
     open System
     open System.Collections
+    open System.Text
     open GeoLocation
+
+    (*
+    * Some introductory material:
+    * - http://www.bigfastblog.com/geohash-intro
+    *)
+
+    exception GeoHashException of string
 
     let DecimalInternalString (v : decimal) =
         let parts = Decimal.GetBits(v)
@@ -14,6 +22,14 @@ module GeoHash =
             parts.[3], parts.[2], parts.[1], parts.[0], isNegative, scale
         )
 
+    let mappings = [|
+        '0'; '1'; '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9';
+        'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'j'; 'k'; 'm';
+        'n'; 'p'; 'q'; 'r'; 's'; 't'; 'u'; 'v'; 'w'; 'x';
+        'y'; 'z'
+    |]
+
+    type GeoTag = | GeoTag of string
 
     type GeoHash = | GeoHash of Latitude:BitArray * Longitude:BitArray
     with
@@ -30,7 +46,7 @@ module GeoHash =
                     if index >= resolution then ()
                     else
                         let midpoint = (min + max) / 2.0m
-                        if angle < min then
+                        if angle < midpoint then
                             result.Set(index, false)
                             assign (index+1) (min, midpoint)
                         else
@@ -46,10 +62,53 @@ module GeoHash =
 
             GeoHash (lat, lon)
 
+        member this.AsBitString =
+            let sb = StringBuilder()
+            let (GeoHash (lat, lon)) = this
+            assert(lat.Count = lon.Count)
+            for i = 0 to (lat.Count - 1) do
+                match lat.[i], lon.[i] with
+                | true, true    -> Printf.bprintf sb "00"
+                | true, false   -> Printf.bprintf sb "01"
+                | false, true   -> Printf.bprintf sb "10"
+                | false, false  -> Printf.bprintf sb "11"
+
+            sb.ToString()
+
+        member this.AsTag =
+            let sb = StringBuilder()
+            let (GeoHash (lat, lon)) = this
+            assert(lat.Count = lon.Count)
+
+            let rec append (latIndex, lonIndex) (isLatNext : bool) (current : int, counter : int) =
+                if counter = 5 then
+                    sb.Append(mappings.[current]) |> ignore
+                    append (latIndex, lonIndex) isLatNext (0, 0)
+                elif counter > 5 then
+                    raise (GeoHashException "Internal error: counter exceeded 5")
+                elif latIndex = lat.Count && lonIndex = lon.Count then
+                    if counter > 0 then sb.Append(mappings.[current]) |> ignore
+                    ()
+                elif latIndex = lat.Count && isLatNext then
+                    // We are done with lat, proceed with lon
+                    append (latIndex, lonIndex) false (current, counter)
+                elif lonIndex = lon.Count && isLatNext = false then
+                    // We are done with lon, proceed with lon
+                    append (latIndex, lonIndex) true (current, counter)
+                elif isLatNext then
+                    let bit = lat.[latIndex]
+                    let current = if bit then (current <<< 1) ||| 0x1 else (current <<< 1)
+                    append (latIndex + 1, lonIndex) false (current, counter + 1)
+                else
+                    // IsLatNext = false
+                    let bit = lon.[lonIndex]
+                    let current = if bit then (current <<< 1) ||| 0x1 else (current <<< 1)
+                    append (latIndex, lonIndex + 1) true (current, counter + 1)
+
+            append (0, 0) false (0, 0)
+            GeoTag (sb.ToString())
+
         member this.AsCoordinates =
             let (GeoHash (lat, lon)) = this
             raise (NotImplementedException())
-
-    GeoHash.Make (Coordinates.MakeFromArcDegreeNaked(51.50642, -0.12721), 8)
-    GeoHash.Make (Coordinates.MakeFromArcDegreeNaked(51.50652, -0.12728), 8)
 
---
+++ PL/DotNet/FSHelper/Generic/Spatial/GeoHashExamples.fsx
@@ -0,0 +1,31 @@
+﻿#load @"../Collections.fs"
+#load "GeoLocation.fs"
+#load "GeoHash.fs"
+
+open CGFSHelper.Spatial.GeoLocation
+open CGFSHelper.Spatial.GeoHash
+
+// See also:
+// - http://geohash.org/
+// - http://geohash.co/
+
+// This should be: u4pruydqqvj
+let jutlang = Coordinates.MakeFromArcDegreeNaked(57.64911, 10.40744)
+
+// This should be: gcpvj07qeu9
+let point1 = Coordinates.MakeFromArcDegreeNaked(51.50642, -0.12721)
+
+// This should be: gcpvj07r4pj
+let point2 = Coordinates.MakeFromArcDegreeNaked(51.50652, -0.12728)
+printfn "Distance: %A" (point1.DistanceTo(point2))
+
+let xx = GeoHash.Make (point1, 20)
+let yy = GeoHash.Make (point2, 20)
+let zz = GeoHash.Make (jutlang, 20)
+
+xx.AsBitString
+yy.AsBitString
+
+xx.AsTag
+yy.AsTag
+zz.AsTag
