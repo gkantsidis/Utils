e7ca10f16126efd127a7b8973086a553da7b02b0
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2019-04-02 15:25:24 +0100

    ported some misc items

--- PL/DotNet/FSHelper/Generic/Collections.fs
+++ PL/DotNet/FSHelper/Generic/Collections.fs
@@ -1,5 +1,6 @@
 ﻿namespace CGFSHelper
 
+/// Extensions and helper methods for some of the built-in collection types.
 module Collections =
     open System
 
@@ -32,3 +33,91 @@ module Collections =
                 if v.HasValue then Nullable(fn v.Value)
                 else Nullable()
 
+    /// Extensions for lists.
+    module List =
+
+        /// <summary>
+        /// Folds over the cross product of two lists
+        /// </summary>
+        /// <param name="folder">The folder function</param>
+        /// <param name="initial">Initial state</param>
+        /// <param name="list1">First list</param>
+        /// <param name="list2">Second list</param>
+        let crossFold (folder : 'State -> 'T1 -> 'T2 -> 'State) (initial : 'State) (list1 : 'T1 list) (list2 : 'T2 list) =
+            let mutable s = initial
+
+            for i = 0 to (list1.Length - 1) do
+                for j = 0 to (list2.Length - 1) do
+                    let item1 = list1.Item i
+                    let item2 = list2.Item j
+                    s <- folder s item1 item2
+
+            s
+
+        /// <summary>
+        /// Generates all unique pairs of items from a list.
+        /// (The order in the pair is not important.)
+        /// </summary>
+        /// <param name="list">The input list</param>
+        let uniquePairs (list : 'T1 list) =
+            seq {
+                for i = 0 to (list.Length - 1) do
+                    for j = (i+1) to (list.Length - 1) do
+                        yield (List.item i list, List.item j list)
+            }
+            |> Seq.toList
+
+        /// <summary>
+        /// Generates all unique pairs of items from a list.
+        /// (The order in the pair is not important.)
+        /// </summary>
+        /// <param name="list">The input list</param>
+        let uniquePairsi (list : 'T1 list) =
+            seq {
+                for i = 0 to (list.Length - 1) do
+                    for j = (i+1) to (list.Length - 1) do
+                        yield (i, List.item i list, j, List.item j list)
+            }
+            |> Seq.toList
+
+        /// <summary>
+        /// For all pairs of items from two lists, compute a function on them
+        /// and return the minimum value.
+        /// </summary>
+        /// <param name="f">The function to compute on the items</param>
+        /// <param name="list1">First list</param>
+        /// <param name="list2">Second list</param>
+        let crossMinBy (f : 'T1 -> 'T2 -> 'T) (list1 : 'T1 list) (list2 : 'T2 list) =
+            let mutable minimum : 'T option = None
+
+            for i = 0 to (list1.Length - 1) do
+                for j = 0 to (list2.Length - 1) do
+                    let item1 = list1.Item i
+                    let item2 = list2.Item j
+                    let value = f item1 item2
+                    if minimum.IsNone
+                    then minimum <- Some value
+                    else minimum <- min minimum.Value value |> Some
+
+            // If any list is empty, the following should throw an exception
+            minimum.Value
+
+        /// <summary>
+        /// Generate a new list, where each new element is pairs with its predecessor.
+        /// (The predecessor of the first element is None.)
+        /// </summary>
+        /// <param name="list">The input list</param>
+        let withPrevious (list : 'T list) =
+            match list with
+            | []        -> []
+            | hd :: tl  ->
+                (None, hd) :: (tl |> List.pairwise |> List.map (fun (previous, current) -> (Some previous, current)))
+
+        /// <summary>
+        /// Apply a filter on a list, but also include in the filtering function the value of the predecessor
+        /// in the input list (the predecessor for the first item is None).
+        /// </summary>
+        /// <param name="fn">The filtering function</param>
+        /// <param name="list">The input list</param>
+        let filterWithPrevious (fn : 'T option * 'T -> bool) (list : 'T list) =
+            list |> withPrevious |> List.filter fn |> List.map snd
---
+++ PL/DotNet/FSHelper/Generic/Runner.fs
@@ -0,0 +1,44 @@
+﻿namespace CGFSHelper
+
+/// Helper methods for running methods repeatively.
+module Runner =
+    /// <summary>
+    /// Run a function a given number of iterations.
+    /// (Typically, the function will change some form of state, or you just want to time it.)
+    /// </summary>
+    /// <param name="iterations">The number of iterations</param>
+    /// <param name="fn">The function to run</param>
+    let repeat iterations (fn : unit -> unit) =
+        for i = 0 to (iterations - 1) do
+            fn ()
+
+    /// <summary>
+    /// Run a function a given number of iterations, and collect the results to a list.
+    /// (Typically, the function will change some form of state.)
+    /// </summary>
+    /// <param name="iterations">The number of iterations</param>
+    /// <param name="fn">The function to run</param>
+    let repeatT<'T> iterations (fn : unit -> 'T) = List.init iterations (fun _ -> fn ())
+
+    /// <summary>
+    /// Run a boolean function multiple times, and count the number of times the function returns true.
+    /// (Typically, the function will change some form of state.)
+    /// </summary>
+    /// <param name="iterations">The number of iterations</param>
+    /// <param name="fn">The function to run</param>
+    let countSuccess<'T> iterations (fn : unit -> bool) =
+        List.init iterations (fun _ -> fn ())
+        |> List.fold (fun (success, fail) result -> if result then (success+1, fail) else (success, fail+1)) (0, 0)
+
+    /// <summary>
+    /// Run a function that returns option multiple times.
+    /// It returns the number of times that the function returned a value, the number of times it returned None,
+    /// as well as all proper values generated.
+    /// (Typically, the function will change some form of state.)
+    /// </summary>
+    /// <param name="iterations"></param>
+    /// <param name="fn"></param>
+    let countSuccessT<'T> iterations (fn : unit -> 'T option) =
+        let result = List.init iterations (fun _ -> fn ()) |> List.choose id
+        let success = result.Length
+        (success, iterations - success), result
