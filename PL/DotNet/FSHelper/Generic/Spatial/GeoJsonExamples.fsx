a64020360e3f6a2ea55e0a9ca392e4ecb2c8b4b9
Christos Gkantsidis <gkantsidis@gmail.com>
2019-04-07 18:42:03 +0100

    basic support for GeoJSON

---
+++ PL/DotNet/FSHelper/Generic/Spatial/GeoJsonExamples.fsx
@@ -0,0 +1,38 @@
+﻿#I @"../../../.paket/load/net47/"
+#load @"FSharp.Data.fsx"
+
+#load @"../Collections.fs"
+#load "GeoLocation.fs"
+#load "GeoJSON.fs"
+
+open CGFSHelper.Spatial
+open GeoJSON
+
+let example1naked =
+    let point = Point.Make(Position.MakeFromArcDegreeNaked(0.5, 102.0))
+    let line =
+        LineString.Make([
+            Position.MakeFromArcDegreeNaked(0.0, 102.0)
+            Position.MakeFromArcDegreeNaked(1.0, 103.0)
+            Position.MakeFromArcDegreeNaked(0.0, 104.0)
+            Position.MakeFromArcDegreeNaked(1.0, 105.0)
+        ])
+    let polygon =
+        Polygon.Make ([
+            Position.MakeFromArcDegreeNaked(0.0, 100.0)
+            Position.MakeFromArcDegreeNaked(0.0, 101.0)
+            Position.MakeFromArcDegreeNaked(1.0, 101.0)
+            Position.MakeFromArcDegreeNaked(1.0, 100.0)
+            Position.MakeFromArcDegreeNaked(0.0, 100.0)
+        ])
+
+    let geometry = [
+        GeometryItem.Point point
+        GeometryItem.LineString line
+        GeometryItem.Polygon polygon
+    ]
+
+    GeometryCollection.Make geometry
+
+let e1nj = Convert.ToJson(example1naked)
+e1nj.ToString()
